8725bf00438797c805f98a0c67da3c56b257ac03
ByeongIk Choi <bichoi0715@gmail.com>
2023-11-16 02:13:51 +0900

    fix: 코드 수정

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,13 +1,14 @@
-//import fs from 'fs';
+import fs from 'fs';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-//import { CERT, FULL_CHAIN, PRIVATE_KEY } from './envList';
-// const httpsOptions = {
-//     ca: fs.readFileSync(FULL_CHAIN),
-//     key: fs.readFileSync(PRIVATE_KEY),
-//     cert: fs.readFileSync(CERT),
-// };
+import { CERT, FULL_CHAIN, PRIVATE_KEY } from './envList';
+const httpsOptions = {
+    ca: fs.readFileSync(FULL_CHAIN),
+    key: fs.readFileSync(PRIVATE_KEY),
+    cert: fs.readFileSync(CERT),
+};
 async function bootstrap() {
+    console.log(httpsOptions);
     const app = await NestFactory.create(AppModule);
     await app.listen(3000);
 }
