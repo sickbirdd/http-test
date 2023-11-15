55e254053f2267f05a11835a9ef55fa983af1aef
ByeongIk Choi <bichoi0715@gmail.com>
2023-11-15 23:40:26 +0900

    feat : HTTPS 서버 코드 작성

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,8 +1,16 @@
+import fs from 'fs';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-
+import { CERT, FULL_CHAIN, PRIVATE_KEY } from './envList';
+const httpsOptions = {
+    ca: fs.readFileSync(FULL_CHAIN),
+    key: fs.readFileSync(PRIVATE_KEY),
+    cert: fs.readFileSync(CERT),
+};
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
+    const app = await NestFactory.create(AppModule, {
+        httpsOptions,
+    });
     await app.listen(3000);
 }
 bootstrap();
