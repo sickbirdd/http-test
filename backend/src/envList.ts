55e0715b15702fa2210ebc3b72e634121f0d164a
ByeongIk Choi <bichoi0715@gmail.com>
2023-11-16 00:55:09 +0900

    fix: 완전히 수정

--- backend/src/envList.ts
+++ backend/src/envList.ts
@@ -1,7 +1,7 @@
-import dotenv from 'dotenv';
-console.log(process.env.NODE_ENV);
-if (process.env.NODE_ENV === 'develop') dotenv.config();
-export const FULL_CHAIN = process.env.FULL_CHAIN || 'asd';
-export const PRIVATE_KEY = process.env.PRIVATE_KEY || 'asda';
-export const CERT = process.env.CERT || 'asda';
-console.log(FULL_CHAIN, PRIVATE_KEY, CERT);
+// import dotenv from 'dotenv';
+// console.log(process.env.NODE_ENV);
+// if (process.env.NODE_ENV === 'develop') dotenv.config();
+// export const FULL_CHAIN = process.env.FULL_CHAIN || 'asd';
+// export const PRIVATE_KEY = process.env.PRIVATE_KEY || 'asda';
+// export const CERT = process.env.CERT || 'asda';
+// console.log(FULL_CHAIN, PRIVATE_KEY, CERT);
