68c9f7bb036a843209d0ef255f837d82865839df
ByeongIk Choi <bichoi0715@gmail.com>
2023-11-16 13:47:08 +0900

    fix: Finish

--- backend/src/envList.ts
+++ backend/src/envList.ts
@@ -1,5 +1,3 @@
 import dotenv from 'dotenv';
 if (process.env.NODE_ENV === 'develop') dotenv.config();
-export const FULL_CHAIN = process.env.FULL_CHAIN as string;
-export const PRIVATE_KEY = process.env.PRIVATE_KEY as string;
-export const CERT = process.env.CERT as string;
+export const DB_HOST = process.env.DB_HOST as string;
