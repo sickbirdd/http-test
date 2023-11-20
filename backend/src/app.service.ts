58afbb7335f93060fc888d18d4a2612545902a36
ByeongIk Choi <bichoi0715@gmail.com>
2023-11-20 23:37:59 +0900

    feat : test

--- backend/src/app.service.ts
+++ backend/src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
     getHello(): string {
-        return 'Welcome to PriceGuard API Server!';
+        return 'Hello World!';
     }
 }
