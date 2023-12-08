d2a48b3208ea7d1885d9fc3fd47040ea40af3f1c
ByeongIk Choi <bichoi0715@gmail.com>
2023-12-08 10:38:10 +0900

    fix: 서버 오류 수정

--- backend/src/product/product.service.ts
+++ backend/src/product/product.service.ts
@@ -11,15 +11,10 @@ import { ProductDetailsDto } from 'src/dto/product.details.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { ProductPrice } from 'src/schema/product.schema';
 import { Model } from 'mongoose';
-import { ProductPriceDto } from 'src/dto/product.price.dto';
 import { PriceDataDto } from 'src/dto/price.data.dto';
-import { CHANNEL_ID, MAX_TRACKING_RANK, NINETY_DAYS, NO_CACHE, THIRTY_DAYS } from 'src/constants';
-import { Cron } from '@nestjs/schedule';
+import { MAX_TRACKING_RANK, NINETY_DAYS, NO_CACHE, THIRTY_DAYS } from 'src/constants';
 import { ProductRankCache } from 'src/utils/cache';
 import { ProductRankCacheDto } from 'src/dto/product.rank.cache.dto';
-import { FirebaseService } from '../firebase/firebase.service';
-import { Message } from 'firebase-admin/lib/messaging/messaging-api';
-import { TrackingProduct } from 'src/entities/trackingProduct.entity';
 import Redis from 'ioredis';
 import { InjectRedis } from '@songkeys/nestjs-redis';
 
@@ -36,7 +31,6 @@ export class ProductService {
         @InjectModel(ProductPrice.name)
         private productPriceModel: Model<ProductPrice>,
         @InjectRedis() private readonly redis: Redis,
-        private readonly firebaseService: FirebaseService,
     ) {
         this.initCache();
     }
@@ -130,11 +124,8 @@ export class ProductService {
         if (trackingProductList.length === 0) return [];
         const trackingListInfo = trackingProductList.map(async ({ product, targetPrice, isAlert }) => {
             const { id, productName, productCode, shop, imageUrl } = product;
-            const cacheData = await this.redis.get(`product:${id}`);
-            const { price } = cacheData
-                ? JSON.parse(cacheData)
-                : await this.productRepository.findOne({ where: { id: id } });
             const priceData = await this.getPriceData(id, THIRTY_DAYS);
+            const { price } = priceData[priceData.length - 1];
             return {
                 productName,
                 productCode,
@@ -154,11 +145,8 @@ export class ProductService {
         const recommendList = this.productRankCache.getAll();
         const recommendListInfo = recommendList.map(async (product, index) => {
             const { id, productName, productCode, shop, imageUrl } = product;
-            const cacheData = await this.redis.get(`product:${id}`);
-            const { price } = cacheData
-                ? JSON.parse(cacheData)
-                : await this.productRepository.findOne({ where: { id: id } });
             const priceData = await this.getPriceData(id, THIRTY_DAYS);
+            const { price } = priceData[priceData.length - 1];
             return {
                 productName,
                 productCode,
@@ -187,7 +175,8 @@ export class ProductService {
         const idx = this.productRankCache.findIndex(selectProduct.id);
         const rank = idx === -1 ? idx : idx + 1;
         const priceData = await this.getPriceData(selectProduct.id, NINETY_DAYS);
-        const { price, lowestPrice } = await this.getProductCurrentData(selectProduct.id);
+        const { price } = priceData[priceData.length - 1];
+        const lowestPrice = Math.min(...priceData.map((item) => item.price));
         return {
             productName: selectProduct.productName,
             shop: selectProduct.shop,
@@ -210,27 +199,25 @@ export class ProductService {
 
     async deleteProduct(userId: string, productCode: string) {
         const product = await this.findTrackingProductByCode(userId, productCode);
-        const prevProduct = this.productRankCache.get(product.productId)?.value;
+        const currentProduct = this.productRankCache.get(product.productId)?.value;
         await this.redis.zincrby('userCount', -1, product.productId);
-
-        if (!prevProduct) {
-            throw new HttpException('상품을 찾을 수 없습니다.', HttpStatus.NOT_FOUND);
-        }
-        prevProduct.userCount--;
-        const productCount = await this.redis.zcard('userCount');
-        if (productCount > MAX_TRACKING_RANK) {
-            await this.deleteUpdateCache(prevProduct);
-        } else {
-            this.productRankCache.update(prevProduct);
+        if (currentProduct) {
+            currentProduct.userCount--;
+            const productCount = await this.redis.zcard('userCount');
+            if (productCount > MAX_TRACKING_RANK) {
+                await this.deleteUpdateCache(currentProduct);
+            } else {
+                this.productRankCache.update(currentProduct);
+            }
         }
         await this.trackingProductRepository.remove(product);
     }
 
-    async deleteUpdateCache(prevProduct: ProductRankCacheDto) {
+    async deleteUpdateCache(currentProduct: ProductRankCacheDto) {
         const nextDataId = (await this.redis.zrevrange('userCount', MAX_TRACKING_RANK, MAX_TRACKING_RANK))[0];
         const cacheData = await this.redis.zscore('userCount', nextDataId);
         const userCount = cacheData ? parseInt(cacheData) : NO_CACHE;
-        if (userCount >= prevProduct.userCount) {
+        if (userCount >= currentProduct.userCount) {
             const newProduct = await this.productRepository.findOne({
                 where: { id: nextDataId },
             });
@@ -245,8 +232,10 @@ export class ProductService {
                 imageUrl: newProduct.imageUrl,
                 userCount: userCount,
             };
-            this.productRankCache.update(prevProduct, newProductRanck);
+            this.productRankCache.update(currentProduct, newProductRanck);
+            return;
         }
+        this.productRankCache.update(currentProduct);
     }
 
     async findTrackingProductByCode(userId: string, productCode: string) {
@@ -263,11 +252,6 @@ export class ProductService {
             throw new HttpException('상품을 찾을 수 없습니다.', HttpStatus.NOT_FOUND);
         }
         return trackingProduct;
-    }
-
-    async mongo(productPriceDto: ProductPriceDto) {
-        const newData = new this.productPriceModel(productPriceDto);
-        return newData.save();
     }
 
     async getPriceData(productId: string, days: number): Promise<PriceDataDto[]> {
@@ -288,123 +272,6 @@ export class ProductService {
         });
     }
 
-    @Cron('* */10 * * * *')
-    async cyclicPriceChecker() {
-        const productList = await this.productRepository.find({ select: { id: true, productCode: true } });
-        const productCodeList = productList.map(({ productCode, id }) => getProductInfo11st(productCode, id));
-        const results = await Promise.all(productCodeList);
-        const updatedDataInfo: ProductInfoDto[] = [];
-        await Promise.all(
-            results.map(async (data) => {
-                const { productId, productPrice, isSoldOut } = data;
-                const cacheData = await this.redis.get(`product:${productId}`);
-                const cache = JSON.parse(cacheData as string);
-                if (!cache || cache.isSoldOut !== isSoldOut || cache.price !== productPrice) {
-                    const lowestPrice = cache ? Math.min(cache.lowestPrice, productPrice) : productPrice;
-                    await this.redis.set(
-                        `product:${productId}`,
-                        JSON.stringify({
-                            isSoldOut,
-                            price: productPrice,
-                            lowestPrice,
-                        }),
-                    );
-                    updatedDataInfo.push(data);
-                }
-            }),
-        );
-        if (updatedDataInfo.length > 0) {
-            await this.productPriceModel.insertMany(
-                updatedDataInfo.map(({ productId, productPrice, isSoldOut }) => {
-                    return { productId, price: productPrice, isSoldOut };
-                }),
-            );
-            const { messages, products } = await this.getNotifications(updatedDataInfo);
-            if (messages.length > 0) {
-                const { responses } = await this.firebaseService.getMessaging().sendEach(messages);
-                const successProducts = products.filter((item, index) => {
-                    const { success } = responses[index];
-                    item.isFirst = false;
-                    return success;
-                });
-                if (successProducts.length > 0) {
-                    await this.trackingProductRepository.save(successProducts);
-                }
-            }
-        }
-    }
-    getMessage(
-        productCode: string,
-        productName: string,
-        productPrice: number,
-        imageUrl: string,
-        token: string,
-    ): Message {
-        return {
-            notification: {
-                title: '목표 가격 이하로 내려갔습니다!',
-                body: `${productName}의 현재 가격은 ${productPrice}원 입니다.`,
-            },
-            data: {
-                productCode,
-            },
-            android: {
-                notification: {
-                    channelId: CHANNEL_ID,
-                    imageUrl,
-                },
-            },
-            token,
-        };
-    }
-    async getNotifications(
-        productInfo: ProductInfoDto[],
-    ): Promise<{ messages: Message[]; products: TrackingProduct[] }> {
-        const productIds = productInfo.map((p) => p.productId);
-
-        const trackingProducts = await this.trackingProductRepository
-            .createQueryBuilder('tracking_product')
-            .where('tracking_product.productId IN (:...productIds)', { productIds })
-            .getMany();
-
-        const trackingMap = new Map<string, TrackingProduct[]>();
-        trackingProducts.forEach((tracking) => {
-            const products = trackingMap.get(tracking.productId) || [];
-            trackingMap.set(tracking.productId, [...products, tracking]);
-        });
-        const results = await Promise.all(
-            productInfo.map(async ({ productId, productCode, productName, productPrice, imageUrl }) => {
-                const trackingList = productId ? trackingMap.get(productId) || [] : [];
-                const notifications = [];
-                const matchedProducts = [];
-
-                for (const trackingProduct of trackingList) {
-                    const { userId, targetPrice, isFirst, isAlert } = trackingProduct;
-                    if (!isFirst && targetPrice < productPrice) {
-                        trackingProduct.isFirst = true;
-                        await this.trackingProductRepository.save(trackingProduct);
-                    } else if (targetPrice >= productPrice && isFirst && isAlert) {
-                        const firebaseToken = await this.redis.get(`firebaseToken:${userId}`);
-                        if (firebaseToken) {
-                            notifications.push(
-                                this.getMessage(productCode, productName, productPrice, imageUrl, firebaseToken),
-                            );
-                            matchedProducts.push(trackingProduct);
-                        }
-                    }
-                }
-                return { notifications, matchedProducts };
-            }),
-        );
-
-        const allNotifications = results.flatMap((result) => result.notifications);
-        const allMatchedProducts = results.flatMap((result) => result.matchedProducts);
-
-        return {
-            messages: allNotifications,
-            products: allMatchedProducts,
-        };
-    }
     async firstAddProduct(productCode: string) {
         const productInfo = await getProductInfo11st(productCode);
         const product = await this.productRepository.saveProduct(productInfo);
